48879264f4f3c607d3e18753f12c1bb6d1cd5a14
Aaron L <aaron@bettercoder.net>
2013-08-06 17:05:30 +0900

    Seperate dependency since it's about to get huge.

---
+++ dependency_test.go
@@ -0,0 +1,106 @@
+package pack
+
+import (
+	"strings"
+	. "testing"
+)
+
+func TestParseDependency(t *T) {
+	t.Parallel()
+	var tests = []struct {
+		Input  string
+		Output Dependency
+		Error  string
+	}{
+		{``, Dependency{}, `empty`},
+		{`name <3.2.5`, Dependency{`name`,
+			[]*Version{&Version{LessThan, 3, 2, 5, ``}}}, ``},
+		{`name <3.2.5 !=1.5.0`, Dependency{`name`,
+			[]*Version{
+				&Version{LessThan, 3, 2, 5, ``},
+				&Version{LessThan, 3, 2, 5, ``},
+			}}, ``},
+		{`1234 a.2.5`, Dependency{}, `name`},
+		{`name1234 a.2`, Dependency{}, `form`},
+	}
+
+	for _, test := range tests {
+		out, err := ParseDependency(test.Input)
+		if err != nil {
+			if len(test.Error) == 0 {
+				t.Error(test)
+				t.Error(test, "had unexpected error:", err)
+			} else if !strings.Contains(err.Error(), test.Error) {
+				t.Error(test, "expected error message like:",
+					test.Error, "got:", err)
+			}
+			continue
+		}
+
+		if out.Name != test.Output.Name {
+			t.Error("Expected:", out.Name, "to be equal to:", test.Output.Name)
+		}
+		if n, e := len(out.Versions), len(test.Output.Versions); n != e {
+			t.Error("Expected:", e, "elements, got:", n)
+		}
+	}
+}
+
+func TestDependency_String(t *T) {
+	t.Parallel()
+	var tests = []struct {
+		Dependency Dependency
+		Expected   string
+	}{
+		{Dependency{}, ""},
+		{Dependency{"", []*Version{&Version{0, 1, 2, 3, ``}}}, ""},
+		{Dependency{"name", nil}, "name"},
+		{Dependency{"name", []*Version{&Version{GreaterThan, 1, 2, 3, ``}}},
+			"name >1.2.3"},
+		{Dependency{"name", []*Version{
+			{GreaterThan, 1, 2, 3, ``},
+			{NotEqual, 1, 5, 0, `pre`},
+		}}, "name >1.2.3 !=1.5.0-pre"},
+	}
+
+	for _, test := range tests {
+		if s := test.Dependency.String(); s != test.Expected {
+			t.Error(test, "expected:", s, "to be equal to:", test.Expected)
+		}
+	}
+}
+
+func TestDependency_GetYAML(t *T) {
+	t.Parallel()
+	d := Dependency{"name", []*Version{&Version{NotEqual, 1, 2, 3, ``}}}
+	_, value := d.GetYAML()
+	if s, ok := value.(string); !ok {
+		t.Error("It should return a string type.")
+	} else if s != "name !=1.2.3" {
+		t.Error("It's not returning the correct string.")
+	}
+}
+
+func TestDependency_SetYAML(t *T) {
+	t.Parallel()
+	var d Dependency
+	if d.SetYAML("", "1234fail") {
+		t.Error("Expecting failure.")
+	}
+	if d.SetYAML("", 10) {
+		t.Error("Expecting failure.")
+	}
+	success := d.SetYAML("", "name >=1.2.3")
+	if !success {
+		t.Error("Expecting success.")
+	}
+	if d.Name != "name" {
+		t.Error("Expected:", d.Name, "to equal: name")
+	}
+	comp := Version{Equal, 1, 2, 3, ``}
+	if len(d.Versions) != 1 {
+		t.Error("Expected a constraint.")
+	} else if !d.Versions[0].Compare(comp) {
+		t.Error("Expected:", d.Versions[0], "to match", comp)
+	}
+}
--- pack.go
+++ pack.go
@@ -5,24 +5,14 @@ to facilitate the reading, writing, and comparison of package metadata.
 package pack
 
 import (
-	"bytes"
 	"errors"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"launchpad.net/goyaml"
-	"regexp"
-	"strings"
-)
-
-const (
-	errFmtDep = `pack: [%v] must be in the form: name( versionconstraint)*`
 )
 
 var (
 	errPartialWrite = errors.New(`pack: Partial write on pack serialization.`)
-	rgxDependency   = regexp.MustCompile(
-		`(?i)^([a-z][a-z0-9_\-]+)((:? [a-z0-9<>=!~\.-]+)+)?$`)
 )
 
 // Author is metadata about an author.
@@ -46,12 +36,6 @@ type Repository struct {
 	// Type can be one of: git/mercurial/bazaar
 	Type string
 	Url  string
-}
-
-// Dependency is a package dependency.
-type Dependency struct {
-	Name     string
-	Versions []*Version
 }
 
 // Pack is the metadata of a package.
@@ -120,68 +104,4 @@ func (p *Pack) WriteTo(writer io.Writer) error {
 	}
 
 	return nil
-}
-
-// ParseDependency parses a string into a Dependency.
-func ParseDependency(str string) (dep Dependency, err error) {
-	if len(str) == 0 {
-		err = errors.New(errMsgEmpty)
-		return
-	}
-
-	parts := rgxDependency.FindStringSubmatch(str)
-	if parts == nil {
-		err = fmt.Errorf(errFmtDep, str)
-		return
-	}
-
-	dep.Name = parts[1]
-	if len(parts[2]) > 0 {
-		splits := strings.Split(parts[2], " ")[1:]
-		ln := len(splits)
-
-		dep.Versions = make([]*Version, ln)
-		for i := 0; i < ln; i++ {
-			dep.Versions[i] = new(Version)
-			*dep.Versions[i], err = ParseVersion(splits[i])
-			if err != nil {
-				return
-			}
-		}
-	}
-	return
-}
-
-// String turns a Dependency into a String.
-func (d *Dependency) String() (str string) {
-	var buf bytes.Buffer
-	if len(d.Name) == 0 {
-		return
-	}
-
-	buf.WriteString(d.Name)
-	if d.Versions != nil {
-		for i := 0; i < len(d.Versions); i++ {
-			buf.WriteByte(' ')
-			buf.WriteString(d.Versions[i].String())
-		}
-	}
-	str = buf.String()
-	return
-}
-
-// GetYAML implements the goyaml Getter interface.
-func (v *Dependency) GetYAML() (_ string, value interface{}) {
-	return "", v.String()
-}
-
-// SetYAML implements the goyaml Setter interface.
-func (v *Dependency) SetYAML(_ string, value interface{}) (ok bool) {
-	var s string
-	var err error
-	if s, ok = value.(string); ok {
-		*v, err = ParseDependency(s)
-		ok = err == nil
-	}
-	return
 }
